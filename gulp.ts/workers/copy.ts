94c00c49b70ed56b710f96ac3ccd217019243a8c
m.sakamaki <mail@m-sakamaki.jp>
2016-02-24 18:21:18 +0900

    add useref

--- gulp.ts/workers/copy.ts
+++ gulp.ts/workers/copy.ts
@@ -1,21 +1,25 @@
 /// <reference path="../../typings/tsd.d.ts"/>
 
 import env from '../env';
-
+(()=>{
+  
 const gulp = require('gulp');
 const replace = require('gulp-replace');
+const useref = require('gulp-useref');
 
-gulp.task('copy:browser', ()=>
-  gulp.src(env.src.browser.html)
-  .pipe(replace(new RegExp('(<!-- jspm:application -->|<script.*</script>)', 'g'), (i)=> {
-    if (i === '<!-- jspm:application -->') {
-      return '<script src="./app.js"></script>';
-    }
+  gulp.task('copy:browser', ()=>
+    gulp.src(env.src.browser.html)
+    // .pipe(replace(new RegExp('(<!-- jspm:application -->|<script.*</script>)', 'g'), (i)=> {
+    //   if (i === '<!-- jspm:application -->') {
+    //     return '<script src="./app.js"></script>';
+    //   }
 
-    return '';
-  }))
-  .pipe(gulp.dest(`${env.dir.compile}/browser`)));
+    //   return '';
+    // }))
+    .pipe(useref())
+    .pipe(gulp.dest(`${env.dir.compile}/browser`)));
 
-gulp.task('copy:assets', ()=>
-  gulp.src(env.src.platform.asset)
-  .pipe(gulp.dest(`${env.dir.compile}/assets`)));
+  gulp.task('copy:assets', ()=>
+    gulp.src(env.src.platform.asset)
+    .pipe(gulp.dest(`${env.dir.compile}/assets`)));
+})();
