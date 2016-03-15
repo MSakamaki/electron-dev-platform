13549e15a5b976e569702d606695b0e48c12bc7e
m.sakamaki <mail@m-sakamaki.jp>
2016-03-15 19:37:55 +0900

    modified #13

--- test/e2e/config/linux.ts
+++ test/e2e/config/linux.ts
@@ -1,12 +1,14 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
+const packageJson = require('../../../package.json');
+
 exports.config = {
   directConnect: true,
   capabilities: {
     browserName: "chrome",
     chromeOptions: {
       // linux x64
-      binary: `${process.cwd()}/dest/pack/exampleApp-linux-x64/exampleApp`,
+      binary: `${process.cwd()}/dest/pack/${packageJson.name}-linux-x64/${packageJson.name}`,
     },  
   }
 };
